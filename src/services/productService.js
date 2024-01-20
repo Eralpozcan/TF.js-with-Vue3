03905b995dfe024f2374147240afb25ae47f2c51
Eralpozcan <eralpozcans@gmail.com>
2024-01-20 05:08:38 +0300

    fix: remove async/await func

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import mockupData from './data.json'
-async function searchByName(query) {
+function searchByName(query) {
   const lowerCaseQuery = query.toLowerCase();
-  return await mockupData.filter(item => item.name.toLowerCase().includes(lowerCaseQuery));
+  return mockupData.filter(item => item.name.toLowerCase().includes(lowerCaseQuery));
 }
 export { searchByName };
